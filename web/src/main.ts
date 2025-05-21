a5354e34fca007b108cd9c4eae10e5237a7753a3
Viktor Mörsell <viktor@upper.st>
2025-05-21 23:49:39 +0200

    make the web app slicker

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,14 +3,17 @@ import "./style.css";
 const WS_URL = "wss://h1uwqodz7a.execute-api.eu-west-1.amazonaws.com/dev/";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h1>Global Volume</h1>
-  <div style="margin:2em 0;">
-    <input id="volume-slider" type="range" min="0" max="100" value="0" disabled style="width:300px;" />
-    <div style="margin-top:1em;font-size:2em;">
-      <span id="volume-value">0</span> / 100
-    </div>
-    <div id="status" style="margin-top:1em;color:gray;font-size:0.9em;"></div>
+  <div id="main-wrapper">
+    <main>
+      <h1>globalvolu.me</h1>
+      <input id="volume-slider" type="range" min="0" max="100" value="0" disabled />
+      <div id="volume-value">0 / 100</div>
+      <div id="status">Connecting...</div>
+    </main>
   </div>
+  <footer>
+    <a href="https://github.com/vmorsell/global-volume/tree/main/client" target="_blank">Python Client</a>
+  </footer>
 `;
 
 const slider = document.getElementById("volume-slider") as HTMLInputElement;
@@ -19,7 +22,7 @@ const status = document.getElementById("status") as HTMLElement;
 
 function setVolume(vol: number) {
   slider.value = vol.toString();
-  valueDisplay.textContent = vol.toString();
+  valueDisplay.textContent = `${vol} / 100`;
 }
 
 function setStatus(msg: string) {
@@ -40,9 +43,7 @@ ws.onmessage = (event) => {
     if (data.volume !== undefined) {
       setVolume(data.volume);
     }
-    if (data.users !== undefined) {
-      setStatus(`${data.users} users connected`);
-    }
+    setStatus(`${data.users || 0} users connected`);
   } catch (e) {
     setStatus("Received invalid message");
   }
